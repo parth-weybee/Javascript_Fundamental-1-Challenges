0ada30486bb994c60342978036f042ce698897f8
parth-weybee <parthmodhvadiya@weybee.in>
2025-08-22 11:51:27 +0530

    Coding Challenge 3 Completed

--- Coding Challenge 1.js
+++ Coding Challenge 1.js
@@ -1,6 +1,6 @@
-const heightMark = 100;
+const heightMark = 1.88;
 const weightMark = 80;
-const heightJone = 100;
+const heightJone = 1.75;
 const weightJone = 70;
 
 const bmiMark = weightMark / (heightMark*heightMark); 
---
+++ Coding Challenge 3.js
@@ -0,0 +1,30 @@
+const dolphine1Game = 97;
+const dolphine2Game = 112;
+const dolphine3Game = 101;
+
+const koalas1Game = 109;
+const koalas2Game = 95;
+const koalas3Game = 106;
+
+const dolphineAvg = (dolphine1Game + dolphine2Game + dolphine3Game) / 3;
+const koalasAvg = (koalas1Game + koalas2Game + koalas3Game) / 3;
+
+if (dolphineAvg > koalasAvg && dolphineAvg >= 100) {
+    console.log("Team Dolphine Wins");
+}
+else if (koalasAvg > dolphineAvg && koalasAvg >= 100) {
+    console.log("Team Koalas Wins")
+}
+else {
+    if(koalasAvg<100)
+    {
+        console.log("Team Dolphine Wins");
+    }
+    else if(dolphineAvg<100)
+    {
+        console.log("Team Koalas Wins");
+    }
+    else{
+        console.log("Draw");
+    }
+}
